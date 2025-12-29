c589f40d5c09ff0fde574e477bcdeae57a3a548e
Bijeta Koirala <bijetakoirala@Bijetas-MacBook-Air.local>
2025-12-29 12:51:04 +0545

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello World Guys, Welcome to python")
+
+
+
+a=20
+b=10
+c= a + b 
+d= 4 + 2j
+print(type(c))
+print(type(d))
+print(a // b)
+print(a+b)
+print(a-b)
+print(a / b)
+print("bijeta" * 10)
